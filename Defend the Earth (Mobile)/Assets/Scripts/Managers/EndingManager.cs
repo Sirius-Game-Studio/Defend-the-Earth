b9b59cb143344f223c1903dbb099617e8cb0585b
WasikFarhanRoopkotha <roopkotha7d@gmail.com>
2019-09-07 13:23:59 +0600

    Change touch detection for credits and add new loading screen

--- Defend the Earth (Mobile)/Assets/Scripts/Managers/EndingManager.cs
+++ Defend the Earth (Mobile)/Assets/Scripts/Managers/EndingManager.cs
@@ -17,12 +17,15 @@ public class EndingManager : MonoBehaviour
     [SerializeField] private Canvas creditsMenu = null;
     [SerializeField] private RectTransform credits = null;
     [SerializeField] private Text moneyCount = null;
-    [SerializeField] private GameObject loadingText = null;
+    [SerializeField] private GameObject loadingScreen = null;
     [SerializeField] private Slider loadingSlider = null;
     [SerializeField] private Text loadingPercentage = null;
+    [SerializeField] private GameObject anyKeyPrompt = null;
+    [SerializeField] private Text loadingTip = null;
 
     private AudioSource audioSource;
     private bool spedupCredits = false;
+    private string currentLoadingTip = "";
     private bool loading = false;
 
     void Awake()
@@ -51,14 +54,14 @@ public class EndingManager : MonoBehaviour
     void Update()
     {
         if (Camera.main.GetComponent<AudioSource>()) Camera.main.GetComponent<AudioSource>().volume = getVolumeData(false);
-        if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.JoystickButton0))
+        if (Input.touchCount > 0)
         {
             if (!spedupCredits)
             {
                 spedupCredits = true;
                 creditsScrollSpeed *= 2;
             }
-        } else if (!Input.GetKey(KeyCode.Mouse0) || !Input.GetKey(KeyCode.JoystickButton0))
+        } else
         {
             if (spedupCredits)
             {
@@ -82,11 +85,13 @@ public class EndingManager : MonoBehaviour
         }
         if (!loading)
         {
-            loadingText.SetActive(false);
+            loadingScreen.SetActive(false);
+            loadingTip.text = "";
             moneyCount.gameObject.SetActive(true);
         } else
         {
-            loadingText.SetActive(true);
+            loadingScreen.SetActive(true);
+            loadingTip.text = currentLoadingTip; 
             moneyCount.gameObject.SetActive(false);
         }
     }
@@ -173,21 +178,31 @@ public class EndingManager : MonoBehaviour
         {
             loading = true;
             AsyncOperation load = SceneManager.LoadSceneAsync(scene);
+            if (LoadingTipArray.instance && LoadingTipArray.instance.tips.Length > 0) currentLoadingTip = LoadingTipArray.instance.tips[Random.Range(0, LoadingTipArray.instance.tips.Length)];
             if (Camera.main.GetComponent<AudioSource>()) Camera.main.GetComponent<AudioSource>().Stop();
             while (!load.isDone)
             {
-                loadingSlider.value = load.progress;
-                loadingPercentage.text = Mathf.Floor(load.progress * 100) + "%";
+                if (load.progress < 0.9f)
+                {
+                    load.allowSceneActivation = false;
+                    loadingSlider.value = load.progress;
+                    loadingPercentage.text = Mathf.Floor(load.progress * 100) + "%";
+                    anyKeyPrompt.SetActive(false);
+                } else
+                {
+                    if (Input.anyKeyDown)
+                    {
+                        loading = false;
+                        load.allowSceneActivation = true;
+                    }
+                    loadingSlider.value = 1;
+                    loadingPercentage.text = "100%";
+                    anyKeyPrompt.SetActive(true);
+                }
                 endingMenu.enabled = false;
                 creditsMenu.enabled = false;
                 yield return null;
             }
-            loading = false;
-            loadingSlider.value = 0;
-            loadingPercentage.text = "0%";
-        } else
-        {
-            StopCoroutine(loadScene(scene));
         }
     }
 }
