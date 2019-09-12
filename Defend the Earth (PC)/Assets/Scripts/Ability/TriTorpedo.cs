aa3febbfd20e8ede33f3d897d9fd357e4997fc06
WasikFarhanRoopkotha <roopkotha7d@gmail.com>
2019-09-12 18:42:11 +0600

    Added a Nightmare-only ability to Alien Destroyer

---
+++ Defend the Earth (PC)/Assets/Scripts/Ability/TriTorpedo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TriTorpedo : MonoBehaviour
+{
+    [SerializeField] private GameObject[] bulletSpawns = new GameObject[0];
+
+    private EnemyGun enemyGun;
+
+    void Start()
+    {
+        enemyGun = GetComponent<EnemyGun>();
+        if (PlayerPrefs.GetInt("Difficulty") >= 4) //Nightmare
+        {
+            foreach (GameObject bulletSpawn in bulletSpawns)
+            {
+                if (bulletSpawn.CompareTag("BulletSpawn")) bulletSpawn.SetActive(true);
+            }
+            enemyGun.damage = (long)(enemyGun.damage * 1.1f);
+        } else //Easy, Normal and Hard
+        {
+            foreach (GameObject bulletSpawn in bulletSpawns)
+            {
+                if (bulletSpawn.CompareTag("BulletSpawn")) bulletSpawn.SetActive(false);
+            }
+        }
+        enabled = false;
+    }
+}
