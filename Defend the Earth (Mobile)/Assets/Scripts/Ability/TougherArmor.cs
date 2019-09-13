917fbab45d44f1f8f7e29162ee8a7560b0df3e09
WasikFarhanRoopkotha <roopkotha7d@gmail.com>
2019-09-13 20:01:39 +0600

    Created Tougher Armor ability script

---
+++ Defend the Earth (Mobile)/Assets/Scripts/Ability/TougherArmor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TougherArmor : MonoBehaviour
+{
+    private EnemyHealth enemyHealth;
+    private EnemyHit enemyHit;
+
+    void Start()
+    {
+        enemyHealth = GetComponent<EnemyHealth>();
+        enemyHit = GetComponent<EnemyHit>();
+        if (PlayerPrefs.GetInt("Difficulty") >= 4) //Nightmare
+        {
+            enemyHealth.health = (long)(enemyHealth.health * 1.25);
+            enemyHealth.defense *= 1.1f;
+            enemyHit.damage = (long)(enemyHit.damage * 1.2);
+        }
+        enabled = false;
+    }
+}
