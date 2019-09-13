fa05f7bd8938a77047f423a447cfad6c263b74cb
WasikFarhanRoopkotha <roopkotha7d@gmail.com>
2019-09-14 00:07:57 +0600

    Created volume slider script

---
+++ Defend the Earth (PC)/Assets/Scripts/Core/SetVolume.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer = null;
+    [SerializeField] private string volume = "";
+
+    private Slider slider;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = PlayerPrefs.GetFloat(volume);
+    }
+
+    public void setVolume()
+    {
+        audioMixer.SetFloat(volume, Mathf.Log10(slider.value) * 20);
+        PlayerPrefs.SetFloat(volume, slider.value);
+        PlayerPrefs.Save();
+    }
+}
