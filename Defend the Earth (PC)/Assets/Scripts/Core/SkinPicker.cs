1af2cbcade02cb02f005e4abcc428084d21b4004
Roopkotha Studio <roopkotha7d@gmail.com>
2019-09-19 19:27:38 +0600

    Added support for randomized emission maps and light colors

--- Defend the Earth (PC)/Assets/Scripts/Core/SkinPicker.cs
+++ Defend the Earth (PC)/Assets/Scripts/Core/SkinPicker.cs
@@ -2,20 +2,25 @@
 
 public class SkinPicker : MonoBehaviour
 {
-    public bool randomized = true;
     public Texture[] textures = new Texture[0];
+    public Texture[] emissionMaps = new Texture[0];
+    public Color32[] lightColors = new Color32[0];
     public int texture = 0;
 
-    private MeshRenderer meshRenderer;
+    private new Renderer renderer;
+    private new Light light;
 
     void Start()
     {
-        meshRenderer = GetComponent<MeshRenderer>();
+        renderer = GetComponent<MeshRenderer>();
+        light = GetComponent<Light>();
         if (textures.Length > 0)
         {
-            if (randomized) texture = Random.Range(0, textures.Length - 1);
-            meshRenderer.material.SetTexture("_MainTex", textures[texture]);
+            texture = Random.Range(0, textures.Length);
+            renderer.material.SetTexture("_MainTex", textures[texture]);
         }
+        if (emissionMaps.Length > 0 && emissionMaps[texture]) renderer.material.SetTexture("_EmissionMap", emissionMaps[texture]);
+        if (light && lightColors.Length > 0) light.color = lightColors[texture];
         enabled = false;
     }
 }
