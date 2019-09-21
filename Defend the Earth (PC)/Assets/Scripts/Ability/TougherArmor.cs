a147395f4a8404a76cec4988d6205cd32db121e3
Roopkotha Studio <roopkotha7d@gmail.com>
2019-09-21 17:31:46 +0600

    Added extra lasers

--- Defend the Earth (PC)/Assets/Scripts/Ability/TougherArmor.cs
+++ Defend the Earth (PC)/Assets/Scripts/Ability/TougherArmor.cs
@@ -2,6 +2,8 @@
 
 public class TougherArmor : MonoBehaviour
 {
+    [SerializeField] private GameObject[] bulletSpawns = new GameObject[0];
+
     private EnemyHealth enemyHealth;
     private EnemyHit enemyHit;
 
@@ -11,9 +13,21 @@ public class TougherArmor : MonoBehaviour
         enemyHit = GetComponent<EnemyHit>();
         if (PlayerPrefs.GetInt("Difficulty") >= 4) //Nightmare
         {
+            foreach (GameObject bulletSpawn in bulletSpawns)
+            {
+                if (bulletSpawn.CompareTag("BulletSpawn")) bulletSpawn.SetActive(true);
+            }
+            bulletSpawns[0].SetActive(false);
             enemyHealth.health = (long)(enemyHealth.health * 1.25);
             enemyHealth.defense *= 1.1f;
             enemyHit.damage = (long)(enemyHit.damage * 1.2);
+        } else
+        {
+            foreach (GameObject bulletSpawn in bulletSpawns)
+            {
+                if (bulletSpawn.CompareTag("BulletSpawn")) bulletSpawn.SetActive(false);
+            }
+            bulletSpawns[0].SetActive(true);
         }
         enabled = false;
     }
