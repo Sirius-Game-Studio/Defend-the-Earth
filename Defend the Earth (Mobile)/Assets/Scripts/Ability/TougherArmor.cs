a19ef7af4cafc34d98cec09c28d319c0e1149e68
Roopkotha Studio <roopkotha7d@gmail.com>
2019-09-23 02:07:54 +0600

    Added 2 extra bullets

--- Defend the Earth (Mobile)/Assets/Scripts/Ability/TougherArmor.cs
+++ Defend the Earth (Mobile)/Assets/Scripts/Ability/TougherArmor.cs
@@ -2,6 +2,8 @@
 
 public class TougherArmor : MonoBehaviour
 {
+    [SerializeField] private GameObject[] bulletSpawns = new GameObject[0];
+
     private EnemyHealth enemyHealth;
     private EnemyHit enemyHit;
 
@@ -11,9 +13,19 @@ public class TougherArmor : MonoBehaviour
         enemyHit = GetComponent<EnemyHit>();
         if (PlayerPrefs.GetInt("Difficulty") >= 4) //Nightmare
         {
+            foreach (GameObject bulletSpawn in bulletSpawns)
+            {
+                if (bulletSpawn.CompareTag("BulletSpawn")) bulletSpawn.SetActive(true);
+            }
             enemyHealth.health = (long)(enemyHealth.health * 1.25);
-            enemyHealth.defense *= 1.1f;
+            enemyHealth.defense *= 0.9f;
             enemyHit.damage = (long)(enemyHit.damage * 1.2);
+        } else
+        {
+            foreach (GameObject bulletSpawn in bulletSpawns)
+            {
+                if (bulletSpawn.CompareTag("BulletSpawn")) bulletSpawn.SetActive(false);
+            }
         }
         enabled = false;
     }
