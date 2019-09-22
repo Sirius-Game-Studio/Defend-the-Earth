801b9b9a75f91721517daf26411a982ba88d906c
Roopkotha Studio <roopkotha7d@gmail.com>
2019-09-23 01:06:59 +0600

    Updated orb split method

--- Defend the Earth (Mobile)/Assets/Scripts/Ability/ChaosOrb.cs
+++ Defend the Earth (Mobile)/Assets/Scripts/Ability/ChaosOrb.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 public class ChaosOrb : MonoBehaviour
 {
     [Header("Settings")]
+    [Tooltip("Chaos Orb fire rate.")] [SerializeField] private float orbFireRate = 0.25f;
     [Tooltip("Chaos Orb movement speed.")] [SerializeField] private float orbSpeed = 1;
-    [Tooltip("How long Chaos Orb lasts for.")] [SerializeField] private float time = 3;
+    [Tooltip("Chaos Orb lifetime.")] [SerializeField] private float lifetime = 3;
     [Tooltip("Projectile damage.")] public long damage = 20;
     [Tooltip("Projectile speed.")] public float speed = 17.5f;
 
     [Header("Setup")]
     [SerializeField] private GameObject sound = null;
     [SerializeField] private GameObject orb = null;
-    [SerializeField] private GameObject split = null;
+    [SerializeField] private GameObject miniOrb = null;
 
     void Start()
     {
         if (PlayerPrefs.GetInt("Difficulty") <= 1) //Easy
         {
-            time *= 1.1f;
+            lifetime *= 1.1f;
         } else if (PlayerPrefs.GetInt("Difficulty") == 3) //Hard
         {
+            orbFireRate *= 0.8f;
             orbSpeed *= 0.95f;
         } else if (PlayerPrefs.GetInt("Difficulty") >= 4) //Nightmare
         {
+            orbFireRate *= 0.6f;
             orbSpeed *= 0.9f;
         }
         StartCoroutine(spreadingLight());
@@ -31,20 +34,19 @@ public class ChaosOrb : MonoBehaviour
 
     void Update()
     {
-        if (time > 0)
+        if (lifetime > 0)
         {
             transform.position += Vector3.up * orbSpeed * Time.deltaTime;
-            time -= Time.deltaTime;
+            lifetime -= Time.deltaTime;
         } else
         {
-            GameObject newSplit = Instantiate(split, transform.position, Quaternion.Euler(0, 0, 0));
-            foreach (Transform projectile in newSplit.transform)
+            float angle = 0;
+            for (int i = 0; i < 24; i++)
             {
-                if (projectile.CompareTag("Projectile") && projectile.GetComponent<EnemyHit>() && projectile.GetComponent<Mover>())
-                {
-                    projectile.GetComponent<EnemyHit>().damage = damage;
-                    projectile.GetComponent<Mover>().speed = speed;
-                }
+                GameObject newOrb = Instantiate(orb, transform.position, Quaternion.Euler(angle, 90, -90));
+                newOrb.GetComponent<EnemyHit>().damage = damage;
+                newOrb.GetComponent<Mover>().speed = speed;
+                angle += 15;
             }
             if (sound) Instantiate(sound, transform.position, transform.rotation);
             Destroy(gameObject);
@@ -55,17 +57,8 @@ public class ChaosOrb : MonoBehaviour
     {
         while (true)
         {
-            if (PlayerPrefs.GetInt("Difficulty") <= 2) //Easy and Normal
-            {
-                yield return new WaitForSeconds(0.25f);
-            } else if (PlayerPrefs.GetInt("Difficulty") == 3) //Hard
-            {
-                yield return new WaitForSeconds(0.2f);
-            } else if (PlayerPrefs.GetInt("Difficulty") >= 4) //Nightmare
-            {
-                yield return new WaitForSeconds(0.15f);
-            }
-            Instantiate(orb, transform.position, Quaternion.Euler(Random.Range(0, 180), 90, -90));
+            yield return new WaitForSeconds(orbFireRate);
+            Instantiate(miniOrb, transform.position, Quaternion.Euler(Random.Range(0, 180), 90, -90));
         }
     }
 }
