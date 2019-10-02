ca88893deda4bd9a11a4b3f7ba6870312b0666ce
Roopkotha Studio <roopkotha7d@gmail.com>
2019-10-03 02:32:33 +0600

    Created ToggleTips script

---
+++ Defend the Earth (Mobile)/Assets/Scripts/Core/ToggleTips.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleTips : MonoBehaviour
+{
+    [SerializeField] private Vector2 enableTextSize = new Vector2(138, 41);
+    [SerializeField] private Vector2 disableTextSize = new Vector2(147, 41);
+    [SerializeField] private AudioClip buttonClick = null;
+
+    private Text toggleText;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        toggleText = GetComponent<Text>();
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource) audioSource.ignoreListenerPause = true;
+    }
+
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("Tips") <= 0)
+        {
+            toggleText.text = "Enable Tips";
+            toggleText.rectTransform.sizeDelta = enableTextSize;
+        } else
+        {
+            toggleText.text = "Disable Tips";
+            toggleText.rectTransform.sizeDelta = disableTextSize;
+        }
+    }
+
+    public void toggleTips()
+    {
+        if (audioSource)
+        {
+            if (buttonClick)
+            {
+                audioSource.PlayOneShot(buttonClick);
+            } else
+            {
+                audioSource.Play();
+            }
+        }
+        if (PlayerPrefs.GetInt("Tips") <= 0)
+        {
+            PlayerPrefs.SetInt("Tips", 1);
+        } else
+        {
+            PlayerPrefs.SetInt("Tips", 0);
+        }
+        PlayerPrefs.Save();
+    }
+}
