43f2ad965032a9145417b17fb4b1fd177a436660
Roopkotha Studio <roopkotha7d@gmail.com>
2019-10-06 19:21:10 +0600

    Hid lives and invulnerable values in Inspector, added min and max values to health, damage and fire rate

--- Defend the Earth (Mobile)/Assets/Scripts/Player/PlayerController.cs
+++ Defend the Earth (Mobile)/Assets/Scripts/Player/PlayerController.cs
@@ -6,9 +6,9 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     [Header("Settings")]
-    public long health = 100;
-    [SerializeField] private long damage = 10;
-    [SerializeField] private float RPM = 150;
+    [Range(50, 250)] public long health = 100;
+    [Range(5, 30)] [SerializeField] private long damage = 10;
+    [Range(100, 300)] [SerializeField] private float RPM = 150;
     [SerializeField] private float speed = 8;
 
     [Header("Powerup Settings")]
@@ -17,19 +17,17 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Supercharge damage multiplier.")] [Range(1.05f, 2)] [SerializeField] private float superchargeMultiplier = 1.5f;
     [Tooltip("Supercharge powerup duration.")] [Range(5, 15)] [SerializeField] private float superchargeTime = 12;
 
+    [Header("Boundary Settings")]
+    [SerializeField] private float yMin = -6.75f;
+    [SerializeField] private float yMax = 2;
+
     [Header("Sound Effects")]
     [SerializeField] private AudioClip fireSound = null;
 
-    [Header("Miscellaneous")]
-    [SerializeField] private float yMin = -6.75f;
-    [SerializeField] private float yMax = 2;
-    public long lives = 3;
-    public bool invulnerable = false;
-
     [Header("Setup")]
-    [SerializeField] private AudioSource lowHealthSound = null;
     [SerializeField] private GameObject bullet = null;
     [SerializeField] private GameObject explosion = null;
+    [SerializeField] private AudioSource lowHealthSound = null;
     [SerializeField] private GameObject textPopup = null;
 
     private new Renderer renderer;
@@ -38,6 +36,8 @@ public class PlayerController : MonoBehaviour
     private Text healthText;
     private Text livesCount;
     private long maxHealth = 0;
+    [HideInInspector] public long lives = 3;
+    [HideInInspector] public bool invulnerable = false;
     private bool hasSupercharge = false;
     private float superchargeDuration = 0;
     private long oldDamage = 0;
@@ -141,24 +141,42 @@ public class PlayerController : MonoBehaviour
             }
             Destroy(gameObject);
         }
-        if (!GameController.instance.gameOver && !GameController.instance.won && !GameController.instance.paused && GameController.instance.pauseButton.color != GameController.instance.pauseButton.GetComponent<ButtonHover>().hoverColor && Input.touchCount > 0)
+        if (!GameController.instance.gameOver && !GameController.instance.won && !GameController.instance.paused && GameController.instance.pauseButton.color != GameController.instance.pauseButton.GetComponent<ButtonHover>().hoverColor)
         {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
-            {
-                Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0));
-                transform.position = Vector3.Lerp(transform.position, newPosition, speed * 0.375f * Time.deltaTime);
-            }
+            #if UNITY_EDITOR
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+            transform.position = Vector3.Lerp(transform.position, newPosition, speed * 0.375f * Time.deltaTime);
             if (Time.time >= nextShot)
             {
-                if (Input.touchCount >= 1 && PlayerPrefs.GetInt("Autofire") >= 1)
+                if (PlayerPrefs.GetInt("Autofire") >= 1)
                 {
                     fire();
-                } else if (Input.touchCount >= 2 && PlayerPrefs.GetInt("Autofire") <= 0)
+                } else
                 {
-                    fire();
+                    if (Input.GetMouseButton(0)) fire();
                 }
             }
+            #else
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+                {
+                    Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0));
+                    transform.position = Vector3.Lerp(transform.position, newPosition, speed * 0.375f * Time.deltaTime);
+                }
+                if (Time.time >= nextShot)
+                {
+                    if (Input.touchCount >= 1 && PlayerPrefs.GetInt("Autofire") >= 1)
+                    {
+                        fire();
+                    } else if (Input.touchCount >= 2 && PlayerPrefs.GetInt("Autofire") <= 0)
+                    {
+                        fire();
+                    }
+                }
+            }
+            #endif
         }
         Vector3 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         float width = GetComponent<Collider>().bounds.extents.x;
