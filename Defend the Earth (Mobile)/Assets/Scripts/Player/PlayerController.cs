daea267a7c767f4c33d127b2b16902231006c55c
Roopkotha Studio <roopkotha7d@gmail.com>
2019-10-08 18:18:33 +0600

    Added active BulletSpawn check

--- Defend the Earth (Mobile)/Assets/Scripts/Player/PlayerController.cs
+++ Defend the Earth (Mobile)/Assets/Scripts/Player/PlayerController.cs
@@ -231,18 +231,18 @@ public class PlayerController : MonoBehaviour
         nextShot = Time.time + 60 / RPM;
         foreach (Transform bulletSpawn in transform)
         {
-            if (bulletSpawn.CompareTag("BulletSpawn"))
+            if (bulletSpawn.CompareTag("BulletSpawn") && bulletSpawn.gameObject.activeSelf)
             {
                 GameObject newBullet = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-                newBullet.GetComponent<BulletHit>().damage = damage;
+                newBullet.GetComponent<Bullet>().damage = damage;
                 foundBulletSpawns = true;
             }
         }
         if (!foundBulletSpawns)
         {
-            GameObject newBullet = Instantiate(bullet, transform.position + new Vector3(0, 1, 0), Quaternion.Euler(-90, 0, 0));
+            GameObject newBullet = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y + 1, 0), Quaternion.Euler(-90, 0, 0));
             if (newBullet.transform.rotation.y != 90) newBullet.transform.rotation = Quaternion.Euler(-90, 0, 0);
-            newBullet.GetComponent<BulletHit>().damage = damage;
+            newBullet.GetComponent<Bullet>().damage = damage;
             foundBulletSpawns = true;
         }
         if (audioSource && foundBulletSpawns)
