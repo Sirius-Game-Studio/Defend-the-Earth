b6479e6cf687fa5d77faac212d5b0ea3f01cd623
Roopkotha Studio <roopkotha7d@gmail.com>
2019-10-13 01:34:54 +0600

    Improved code

--- Defend the Earth (PC)/Assets/Scripts/Core/ButtonHover.cs
+++ Defend the Earth (PC)/Assets/Scripts/Core/ButtonHover.cs
@@ -11,39 +11,46 @@ public class ButtonHover : MonoBehaviour
     private Text text;
     private Image image;
     private Outline outline;
+    private bool hovering = false;
 
-    void Start()
+    void OnEnable()
     {
-        if (!isImage)
-        {
-            text = GetComponent<Text>();
-            outline = GetComponent<Outline>();
-            text.color = normalColor;
-            if (outline) outline.effectColor = new Color32((byte)(normalColor.r * 0.5), (byte)(normalColor.g * 0.5), (byte)(normalColor.b * 0.5), 255);
-        } else
-        {
-            image = GetComponent<Image>();
-            image.color = normalColor;
-        }
-        if (textsToShow.Length > 0)
-        {
-            foreach (Text t in textsToShow) if (t) t.enabled = false;
-        }
+        text = GetComponent<Text>();
+        image = GetComponent<Image>();
+        outline = GetComponent<Outline>();
+        hovering = false;
+        setState(false);
+        CancelInvoke("onClickColor");
+    }
+
+    void OnDisable()
+    {
+        hovering = false;
+        setState(false);
+        CancelInvoke("onClickColor");
     }
 
     public void OnMouseEnter()
     {
+        hovering = true;
         setState(true);
     }
 
     public void OnMouseExit()
     {
+        hovering = false;
         setState(false);
     }
 
     public void OnMouseClick()
     {
         setState(false);
+        CancelInvoke("onClickColor");
+        Invoke("onClickColor", 0.15f);
+        if (textsToShow.Length > 0)
+        {
+            foreach (Text t in textsToShow) if (t) t.enabled = true;
+        }
     }
 
     void setState(bool state)
@@ -75,6 +82,21 @@ public class ButtonHover : MonoBehaviour
             if (textsToShow.Length > 0)
             {
                 foreach (Text t in textsToShow) if (t) t.enabled = false;
+            }
+        }
+    }
+
+    void onClickColor()
+    {
+        if (hovering)
+        {
+            if (!isImage)
+            {
+                text.color = hoverColor;
+                if (outline) outline.effectColor = new Color32((byte)(hoverColor.r * 0.5), (byte)(hoverColor.g * 0.5), (byte)(hoverColor.b * 0.5), 255);
+            } else
+            {
+                image.color = hoverColor;
             }
         }
     }
