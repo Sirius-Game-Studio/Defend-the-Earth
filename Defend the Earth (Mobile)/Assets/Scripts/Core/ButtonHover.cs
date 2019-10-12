0dec344970d1d574f994273baf02f5b84370e034
Roopkotha Studio <roopkotha7d@gmail.com>
2019-10-12 16:08:27 +0600

    Added support for Image buttons

--- Defend the Earth (Mobile)/Assets/Scripts/Core/ButtonHover.cs
+++ Defend the Earth (Mobile)/Assets/Scripts/Core/ButtonHover.cs
@@ -3,19 +3,28 @@ using UnityEngine.UI;
 
 public class ButtonHover : MonoBehaviour
 {
-    public Color32 normalColor = new Color32(200, 200, 200, 255);
-    public Color32 hoverColor = new Color32(255, 255, 255, 255);
+    [SerializeField] private bool isImage = false;
+    [SerializeField] private Color32 normalColor = new Color32(200, 200, 200, 255);
+    [SerializeField] private Color32 hoverColor = new Color32(255, 255, 255, 255);
     [SerializeField] private Text[] textsToShow = new Text[0];
 
     private Text text;
+    private Image image;
     private Outline outline;
 
     void Start()
     {
-        text = GetComponent<Text>();
-        outline = GetComponent<Outline>();
-        text.color = normalColor;
-        if (outline) outline.effectColor = new Color32((byte)(normalColor.r * 0.5), (byte)(normalColor.g * 0.5), (byte)(normalColor.b * 0.5), 255);
+        if (!isImage)
+        {
+            text = GetComponent<Text>();
+            outline = GetComponent<Outline>();
+            text.color = normalColor;
+            if (outline) outline.effectColor = new Color32((byte)(normalColor.r * 0.5), (byte)(normalColor.g * 0.5), (byte)(normalColor.b * 0.5), 255);
+        } else
+        {
+            image = GetComponent<Image>();
+            image.color = normalColor;
+        }
         if (textsToShow.Length > 0)
         {
             foreach (Text t in textsToShow) if (t) t.enabled = false;
@@ -24,8 +33,14 @@ public class ButtonHover : MonoBehaviour
 
     public void OnMouseEnter()
     {
-        text.color = hoverColor;
-        if (outline) outline.effectColor = new Color32((byte)(hoverColor.r * 0.5), (byte)(hoverColor.g * 0.5), (byte)(hoverColor.b * 0.5), 255);
+        if (!isImage)
+        {
+            text.color = hoverColor;
+            if (outline) outline.effectColor = new Color32((byte)(hoverColor.r * 0.5), (byte)(hoverColor.g * 0.5), (byte)(hoverColor.b * 0.5), 255);
+        } else
+        {
+            image.color = hoverColor;
+        }
         if (textsToShow.Length > 0)
         {
             foreach (Text t in textsToShow) if (t) t.enabled = true;
@@ -34,8 +49,14 @@ public class ButtonHover : MonoBehaviour
 
     public void OnMouseExit()
     {
-        text.color = normalColor;
-        if (outline) outline.effectColor = new Color32((byte)(normalColor.r * 0.5), (byte)(normalColor.g * 0.5), (byte)(normalColor.b * 0.5), 255);
+        if (!isImage)
+        {
+            text.color = normalColor;
+            if (outline) outline.effectColor = new Color32((byte)(normalColor.r * 0.5), (byte)(normalColor.g * 0.5), (byte)(normalColor.b * 0.5), 255);
+        } else
+        {
+            image.color = normalColor;
+        }
         if (textsToShow.Length > 0)
         {
             foreach (Text t in textsToShow) if (t) t.enabled = false;
