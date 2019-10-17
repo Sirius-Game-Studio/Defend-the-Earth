1a678c74bfdb376ba67446eb37bfc43cf38f2fa0
Roopkotha Studio <roopkotha7d@gmail.com>
2019-10-17 19:49:17 +0600

    Created Money Counter script

---
+++ Defend the Earth (Mobile)/Assets/Scripts/Core/MoneyCount.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyCount : MonoBehaviour
+{
+    private Text main;
+
+    void Start()
+    {
+        main = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (PlayerPrefs.GetString("Money") != "")
+        {
+            main.text = "$" + PlayerPrefs.GetString("Money");
+        } else
+        {
+            main.text = "$0";
+        }
+    }
+}
